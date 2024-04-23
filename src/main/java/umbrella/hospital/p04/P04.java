b7c130e228a3578942669c24b042497a243d7c56
winterevil <long.lethanh.cit21@eiu.edu.vn>
2024-04-23 15:45:09 +0700

    long1stcommit

---
+++ src/main/java/umbrella/hospital/p04/P04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package umbrella.hospital.p04;
+
+/**
+ *
+ * @author Admin
+ */
+public class P04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
